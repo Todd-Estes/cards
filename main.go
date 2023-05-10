320f67b1aaa8200c1957786db9f25a888d525e59
todd-estes <elestes@gmail.com>
2023-05-09 20:27:37 -0400

    update deck and main

--- main.go
+++ main.go
@@ -1,16 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
-	fmt.Println(cards)
-
-	for i, card := range cards {
-		fmt.Print(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
